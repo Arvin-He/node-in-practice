1ea7a52528cc03c02d10a8bf35fdd2f726c62c79
hippo <510205617@qq.com>
2018-11-24 16:39:02 +0800

    add node notes

--- lesson-06/main.js
+++ lesson-06/main.js
@@ -1,19 +1,25 @@
-// 学习使用测试框架 mocha : http://mochajs.org/
-// 学习使用断言库 should : https://github.com/tj/should.js
-// 学习使用测试率覆盖工具 istanbul : https://github.com/gotwarlost/istanbul
-// 简单 Makefile 的编写 : http://blog.csdn.net/haoel/article/details/2886
-
 var fibonacci = function (n) {
+    if (typeof n !== 'number') {
+        throw new Error('n should be a Number')
+    }
+    if (n < 0) {
+        throw new Error('n should >= 0')
+    }
+    if (n > 10) {
+        throw new Error('n should <= 10')
+    }
     if (n === 0) {
         return 0
     }
     if (n === 1) {
         return 1
     }
-    return fibonacci(n-1) + fibonacci(n-2)
+    return fibonacci(n - 1) + fibonacci(n - 2)
 }
 
 if (require.main === module) {
     var n = Number(process.argv[2])
-    console.log('fibonacci(' + n +') is', fibonacci(n))
+    console.log('fibonacci(' + n + ') is', fibonacci(n))
 }
+
+exports.fibonacci = fibonacci
